7c631fb678e04d65f73391793c6a58b5bd4a597c
CM Soyza <cmsoyza@gmail.com>
2024-09-01 18:31:18 +0530

    Loading screen related changes made with react-spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
     const [jobs, setJobs] = useState([]);
-    // const jobListing = isHome ? jobs.slice(0, 3) : jobs;
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        const apiUrl = isHome ? 'http://localhost:8080/jobs?_limit=3' : 'http://localhost:8080/jobs';
         const fetchJobs = async () => {
             try {
-                const res = await fetch('http://localhost:8080/jobs');
+                const res = await fetch(apiUrl);
                 const data = await res.json();
                 setJobs(data);
             } catch (error) {
                 console.log('Error data fetching: ' + error);
-            }finally{
+            } finally {
                 setLoading(false);
             }
         }
@@ -26,11 +27,18 @@ const JobListings = ({ isHome = false }) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobs.map((job) => (
-                        <JobListing key={job.id} job={job} />
-                    ))}
-                </div>
+
+                {loading ? (<Spinner loading={loading} />)
+                    :
+                    (
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {jobs.map((job) => (
+                                <JobListing key={job.id} job={job} />
+                            ))}
+                        </div>
+                    )
+                }
+
             </div>
         </section>
     )
