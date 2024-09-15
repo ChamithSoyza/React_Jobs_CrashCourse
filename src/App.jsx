8ad01ce60fe726615f8cb4726ddde7a505d787d5
CM Soyza <cmsoyza@gmail.com>
2024-09-15 23:30:23 +0530

    Form submition related changes made.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import PageNotFound from './pages/PageNotFound';
-import JobPage, {jobLoader} from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
+
 const App = () => {
+  const addJob = (newJob) => {
+    console.log('addJobSubmit -> New Job ' + newJob);
+  }
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
         <Route path='*' element={<PageNotFound />} />
-        <Route path='/add-job' element={<AddJobPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
       </Route>
     )
   );
