ed18ce1a944d79108d2a89e62499985a20974cc0
CM Soyza <cmsoyza@gmail.com>
2024-09-18 13:55:39 +0530

    Created edit job page and update job lisitng functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import JobsPage from './pages/JobsPage';
 import PageNotFound from './pages/PageNotFound';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 
 const App = () => {
@@ -28,12 +29,26 @@ const App = () => {
     });
     return;
   }
+
+  //UPDATE A JOB
+  const updateJob = async (job) => {
+    const response = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
         <Route path='*' element={<PageNotFound />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
       </Route>
